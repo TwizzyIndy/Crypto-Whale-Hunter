4a12e3b520731cccc15c42362c84e63a00d19164
TwizzyIndy <twizzy.indy@gmail.com>
2025-02-21 15:17:04 +0630

    added some comments

--- style.py
+++ style.py
@@ -1,14 +1,16 @@
 def satirYaz(market):
+    # Calculate the volume change ratio. If greater than 1, there is an increase; if less than 1, a decrease.
     hacim_deg= market.hacim_degisim()
     if hacim_deg > 1.5:
-        color = '\x1b[6;30;42m' 
+        color = '\x1b[6;30;42m' # Green
     elif hacim_deg > 1:
-        color = '\x1b[5;30;43m'
+        color = '\x1b[5;30;43m' # Yellow
     elif hacim_deg > 0.6:
-        color = '\x1b[6;30;47m'
+        color = '\x1b[6;30;47m' # White
     else:
-        color = '\x1b[6;31;47m'
-         
+        color = '\x1b[6;31;47m' # Red
+
+    # Calculate the price change ratio. If greater than 3, there is an increase; if less than 3, a decrease.
     artis_deg= market.fiyat_artis_azalis()
     if artis_deg >= 3:
         color_artis = '\x1b[6;30;42m'
@@ -23,7 +25,7 @@ def satirYaz(market):
         else:
             icerik = '  '
     
-    print ( market.df24['symbol'][0:6], end='\t\t' )
+    print ( market.df24['symbol'], end='\t\t' )
     print ( color + str(  "{:.4f}".format(market.hacim_degisim())  ) + '\x1b[0m', end='\t' )
     print ( color_artis + icerik + '\x1b[0m' , end='\t'  )
     print ( str(int(market.recent_trades_time_interval())) + 'sec')
